718f510652ecc98626c6130d940c89371423441d
Yamina Boukari <87201452+YaminaB@users.noreply.github.com>
2026-07-13 15:29:24 +0000

    added rounded redacted ns to median calculations

--- analysis/census_cohort_age_summary.R
+++ analysis/census_cohort_age_summary.R
@@ -29,6 +29,12 @@ cohort <- read_feather(cohort_file) %>%
     )
   )
 
+rounding <- function(vars) {
+  case_when(vars == 0 ~ 0,
+            vars > 7 ~ round(vars / 5) * 5)
+}
+
+
 table_median_iqr <- cohort %>%
   pivot_longer(
     #cols = all_of(mig_vars),
@@ -46,6 +52,21 @@ table_median_iqr <- cohort %>%
     q25_age = quantile(age_on_census_date, 0.25, na.rm = TRUE),
     q75_age = quantile(age_on_census_date, 0.75, na.rm = TRUE)) %>%
   ungroup() 
+
+totals <- cohort %>%
+  pivot_longer(
+    #cols = all_of(mig_vars),
+    cols = all_of("mig_status_3_cat"),
+    names_to = "migration_scheme",
+    values_to = "migration_status"
+  ) %>%
+  group_by(migration_status) %>%
+  count(migration_status,
+        name = "n") %>%
+  mutate(n_rounded_redacted = rounding(n))
+
+table_median_iqr <- table_median_iqr %>%
+  left_join(totals, by = "migration_status")
 
 dir_create(path_dir(output_file))
 write_csv(table_median_iqr, path = output_file)
