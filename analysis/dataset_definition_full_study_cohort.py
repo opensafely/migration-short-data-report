94ae1d56fe38ef5963d0c986f82ddd081de47c6a
Yamina Boukari <87201452+YaminaB@users.noreply.github.com>
2026-08-03 12:43:04 +0000

    added date of first migration code variable to full cohort dataset

--- analysis/dataset_definition_full_study_cohort.py
+++ analysis/dataset_definition_full_study_cohort.py
@@ -167,6 +167,16 @@ dataset.mig_status_6_cat_withdoe = migration_status_variables.build_mig_status_6
     migrant_indicators
 )
 
+# date of first migration code with no date restrictions 
+
+date_of_first_migration_code_no_restrictions = (
+    clinical_events
+            .where(clinical_events.snomedct_code.is_in(codelists.all_migrant_codes))
+            .sort_by(clinical_events.date)
+            .first_for_patient().date
+
+)
+
 # number of migration codes per person
 
 number_of_migration_codes = (
